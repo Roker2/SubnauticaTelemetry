63bea9e9e4612191a15ecedd1a31c21809cf190e
Roker2 <dimaminko2000@gmail.com>
2022-12-05 14:40:48 +0300

    Add project files.
    Based on https://github.com/MrPurple6411/MrPurple6411-Subnautica-Mods

---
+++ SubnauticaTeslasuit/Main.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+using System.Reflection;
+
+namespace SubnauticaTeslasuit
+{
+    [QModCore]
+    public class Main
+    {
+        [QModPatch]
+        public static void Load()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            new Harmony($"Roker2_{assembly.GetName().Name}").PatchAll(assembly);
+        }
+    }
+}
