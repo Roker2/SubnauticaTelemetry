c6799e25ed227909f7d6d6c4c358b6fc9ea22b6a
Roker2 <dimaminko2000@gmail.com>
2022-12-05 15:15:22 +0300

    Add mod settings menu

---
+++ SubnauticaTeslasuit/Configuration/Config.cs
@@ -0,0 +1,11 @@
+﻿using SMLHelper.V2.Json;
+using SMLHelper.V2.Options.Attributes;
+
+namespace SubnauticaTeslasuit.Configuration
+{
+    [Menu("Teslasuit Force Feedback")]
+    class Config: ConfigFile
+    {
+
+    }
+}
--- SubnauticaTeslasuit/Main.cs
+++ SubnauticaTeslasuit/Main.cs
@@ -1,12 +1,16 @@
 ﻿using HarmonyLib;
 using QModManager.API.ModLoading;
 using System.Reflection;
+using SMLHelper.V2.Handlers;
+using SubnauticaTeslasuit.Configuration;
 
 namespace SubnauticaTeslasuit
 {
     [QModCore]
     public class Main
     {
+        internal static Config Config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+
         [QModPatch]
         public static void Load()
         {
