660600ee6d34b4dfa11c913c756cae6468c1267c
Roker2 <dimaminko2000@gmail.com>
2022-12-05 20:33:33 +0300

    Add PlayerUpdatePatch and needed dependencies

---
+++ SubnauticaTeslasuit/Patches/PlayerUpdatePatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace SubnauticaTeslasuit.Patches
+{
+    [HarmonyPatch(typeof(Player), nameof(Player.Update))]
+    class PlayerUpdatePatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            float playerDepth = Ocean.main.GetDepthOf(Player.main.gameObject);
+        }
+    }
+}
