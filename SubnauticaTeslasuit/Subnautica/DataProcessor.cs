aec11cf6e62e1d646c57c4e77a9ec3434effe8d2
Roker2 <dimaminko2000@gmail.com>
2022-12-08 00:51:41 +0300

    Subnautica: DataProcessor: Add processors and water pressure event processing

--- SubnauticaTeslasuit/Subnautica/DataProcessor.cs
+++ SubnauticaTeslasuit/Subnautica/DataProcessor.cs
@@ -1,7 +1,11 @@
-﻿namespace SubnauticaTeslasuit.Subnautica
+﻿using SubnauticaTeslasuit.ForceFeedback;
+using System.Collections.Generic;
+
+namespace SubnauticaTeslasuit.Subnautica
 {
     class DataProcessor
     {
+        List<IForceFeedbackProcessor> Processors = new List<IForceFeedbackProcessor>();
         // TODO: move it to Teslasuit IForceFeedbackProcessor realization
         private float MinPressureFrequenceMultiplier = 0.5f;
         private float MaxPressureFrequenceMultiplier = 1;
@@ -11,6 +15,16 @@
                 return;
             depth = Normalize(depth, Consts.MinOceanDepth, Consts.MaxOceanDepth);
             float pressureFrequenceMultiplier = CalculateFrequenceMultiplier(CalculateDepthPrecent(depth));
+            ForceFeedbackEvent forceFeedbackEvent = new ForceFeedbackEvent(ForceFeedbackType.WaterPressure, pressureFrequenceMultiplier, true);
+            foreach (var processor in Processors)
+            {
+                processor.ProcessEvent(forceFeedbackEvent);
+            }
+        }
+
+        public void AddForceFeedbackProcessor(IForceFeedbackProcessor processor)
+        {
+            Processors.Add(processor);
         }
 
         private float CalculateDepthPrecent(float depth)
