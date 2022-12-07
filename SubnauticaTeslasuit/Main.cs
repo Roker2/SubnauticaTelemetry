34eb26752c0deed1ab33db1f5741ee5c600084fe
Roker2 <dimaminko2000@gmail.com>
2022-12-08 01:33:31 +0300

    Main: Add data processor and ff processor

--- SubnauticaTeslasuit/Main.cs
+++ SubnauticaTeslasuit/Main.cs
@@ -3,6 +3,8 @@ using QModManager.API.ModLoading;
 using System.Reflection;
 using SMLHelper.V2.Handlers;
 using SubnauticaTeslasuit.Configuration;
+using SubnauticaTeslasuit.Subnautica;
+using SubnauticaTeslasuit.Mock;
 
 namespace SubnauticaTeslasuit
 {
@@ -11,11 +13,14 @@ namespace SubnauticaTeslasuit
     {
         internal static Config Config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
 
+        internal static DataProcessor dataProcessor = new DataProcessor();
+
         [QModPatch]
         public static void Load()
         {
             var assembly = Assembly.GetExecutingAssembly();
             new Harmony($"Roker2_{assembly.GetName().Name}").PatchAll(assembly);
+            dataProcessor.AddForceFeedbackProcessor(new MockForceFeedbackProcessor());
         }
     }
 }
