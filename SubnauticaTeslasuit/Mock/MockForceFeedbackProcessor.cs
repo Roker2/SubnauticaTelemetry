72560848f81f498787b36da3d804806eb96c726b
Roker2 <dimaminko2000@gmail.com>
2022-12-08 01:17:21 +0300

    Mock: MockForceFeedbackProcessor: Init

---
+++ SubnauticaTeslasuit/Mock/MockForceFeedbackProcessor.cs
@@ -0,0 +1,44 @@
+﻿using SubnauticaTeslasuit.ForceFeedback;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SubnauticaTeslasuit.Mock
+{
+    class MockForceFeedbackProcessor : IForceFeedbackProcessor
+    {
+        private string LogFileName = "log.txt";
+
+        public MockForceFeedbackProcessor()
+        {
+            FileInfo fileInfo = new FileInfo(LogFileName);
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+        }
+
+        public void ProcessEvent(ForceFeedbackEvent ffevent)
+        {
+            using (StreamWriter writetext = new StreamWriter(LogFileName, true))
+            {
+                writetext.WriteLine(ffevent.ToString());
+            }
+        }
+
+        public void ProcessEvents(ForceFeedbackEvent[] ffevents)
+        {
+            foreach (var ffevent in ffevents)
+            {
+                ProcessEvent(ffevent);
+            }
+        }
+
+        public void ProcessEvents(List<ForceFeedbackEvent> ffevents)
+        {
+            foreach (var ffevent in ffevents)
+            {
+                ProcessEvent(ffevent);
+            }
+        }
+    }
+}
