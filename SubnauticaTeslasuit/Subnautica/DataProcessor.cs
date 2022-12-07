76725e7605726ea7f467d21b2f037fc74d05d6d4
Roker2 <dimaminko2000@gmail.com>
2022-12-07 21:08:51 +0300

    Subnautica: DataProcessor: Calculate pressure frequence multiplier

--- SubnauticaTeslasuit/Subnautica/DataProcessor.cs
+++ SubnauticaTeslasuit/Subnautica/DataProcessor.cs
@@ -2,9 +2,22 @@
 {
     class DataProcessor
     {
+        private float MinPressureFrequenceMultiplier = 0.5f;
+        private float MaxPressureFrequenceMultiplier = 1;
         public void ProcessPlayerDepth(float depth)
         {
             depth = Normalize(depth, Consts.MinOceanDepth, Consts.MaxOceanDepth);
+            float pressureFrequenceMultiplier = CalculateFrequenceMultiplier(CalculateDepthPrecent(depth));
+        }
+
+        private float CalculateDepthPrecent(float depth)
+        {
+            return (depth - Consts.MinOceanDepth) / (Consts.MaxOceanDepth - Consts.MinOceanDepth);
+        }
+
+        private float CalculateFrequenceMultiplier(float depthPrecent)
+        {
+            return MinPressureFrequenceMultiplier + (MaxPressureFrequenceMultiplier - MinPressureFrequenceMultiplier) * depthPrecent;
         }
 
         private float Normalize(float value, float min, float max)
