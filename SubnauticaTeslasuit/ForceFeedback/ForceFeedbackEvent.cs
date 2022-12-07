fadcc4d0ff0ffd0c95ead8d5de2367e48f971a4b
Roker2 <dimaminko2000@gmail.com>
2022-12-08 00:42:04 +0300

    ForceFeedback: ForceFeedbackEvent: Add constructor, Replay mode and method toString

--- SubnauticaTeslasuit/ForceFeedback/ForceFeedbackEvent.cs
+++ SubnauticaTeslasuit/ForceFeedback/ForceFeedbackEvent.cs
@@ -2,7 +2,20 @@
 {
     struct ForceFeedbackEvent
     {
-        ForceFeedbackType Type;
-        float Multiplier;
+        public ForceFeedbackType Type;
+        public float Multiplier;
+        public bool Replay;
+
+        public ForceFeedbackEvent(ForceFeedbackType type, float multiplier, bool replay)
+        {
+            Type = type;
+            Multiplier = multiplier;
+            Replay = replay;
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {Type}, Multiplier: {Multiplier}, Replay: {Replay}";
+        }
     }
 }
