632b54d153ed9e6eb17b6ad44af72a5b7d47f0d1
Roker2 <dimaminko2000@gmail.com>
2022-12-08 00:47:46 +0300

    ForceFeedback: IForceFeedbackProcessor: Add one more method ProcessEvents

--- SubnauticaTeslasuit/ForceFeedback/IForceFeedbackProcessor.cs
+++ SubnauticaTeslasuit/ForceFeedback/IForceFeedbackProcessor.cs
@@ -1,8 +1,11 @@
-﻿namespace SubnauticaTeslasuit.ForceFeedback
+﻿using System.Collections.Generic;
+
+namespace SubnauticaTeslasuit.ForceFeedback
 {
     interface IForceFeedbackProcessor
     {
         void ProcessEvents(ForceFeedbackEvent[] ffevents);
+        void ProcessEvents(List<ForceFeedbackEvent> ffevents);
         void ProcessEvent(ForceFeedbackEvent ffevent);
     }
 }
