9a14019d4c26ae5ccbfca171834aec98df027ca5
Roker2 <dimaminko2000@gmail.com>
2022-12-08 13:11:59 +0300

    Subnautica: DataProcessor: Remove specific code

--- SubnauticaTeslasuit/Subnautica/DataProcessor.cs
+++ SubnauticaTeslasuit/Subnautica/DataProcessor.cs
@@ -6,16 +6,13 @@ namespace SubnauticaTeslasuit.Subnautica
     class DataProcessor
     {
         List<IForceFeedbackProcessor> Processors = new List<IForceFeedbackProcessor>();
-        // TODO: move it to Teslasuit IForceFeedbackProcessor realization
-        private float MinPressureFrequenceMultiplier = 0.5f;
-        private float MaxPressureFrequenceMultiplier = 1;
         public void ProcessPlayerDepth(float depth)
         {
             if (!Main.Config.enableWaterPressureEffect)
                 return;
             depth = Normalize(depth, Consts.MinOceanDepth, Consts.MaxOceanDepth);
-            float pressureFrequenceMultiplier = CalculateFrequenceMultiplier(CalculateDepthPrecent(depth));
-            ForceFeedbackEvent forceFeedbackEvent = new ForceFeedbackEvent(ForceFeedbackType.WaterPressure, pressureFrequenceMultiplier, true);
+            float depthPrecent = CalculateDepthPrecent(depth);
+            ForceFeedbackEvent forceFeedbackEvent = new ForceFeedbackEvent(ForceFeedbackType.WaterPressure, depthPrecent, true);
             foreach (var processor in Processors)
             {
                 processor.ProcessEvent(forceFeedbackEvent);
@@ -30,12 +27,6 @@ namespace SubnauticaTeslasuit.Subnautica
         private float CalculateDepthPrecent(float depth)
         {
             return (depth - Consts.MinOceanDepth) / (Consts.MaxOceanDepth - Consts.MinOceanDepth);
-        }
-
-        // TODO: move it to Teslasuit IForceFeedbackProcessor realization
-        private float CalculateFrequenceMultiplier(float depthPrecent)
-        {
-            return MinPressureFrequenceMultiplier + (MaxPressureFrequenceMultiplier - MinPressureFrequenceMultiplier) * depthPrecent;
         }
 
         private float Normalize(float value, float min, float max)
