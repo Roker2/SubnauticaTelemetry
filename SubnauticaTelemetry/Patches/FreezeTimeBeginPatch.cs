436b0bad8412e81d06d96aece0353e35cf990f91
Roker2 <dimaminko2000@gmail.com>
2022-12-10 03:32:19 +0300

    Patches: Add patches to FreezeTime class
    Universal method to detect game pause

---
+++ SubnauticaTelemetry/Patches/FreezeTimeBeginPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UWE;
+
+namespace SubnauticaTelemetry.Patches
+{
+    [HarmonyPatch(typeof(FreezeTime), nameof(FreezeTime.Begin))]
+    class FreezeTimeBeginPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            Main.dataProcessor.running = false;
+            Main.dataProcessor.StopAllEvents();
+        }
+    }
+}
---
+++ SubnauticaTelemetry/Patches/FreezeTimeEndPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using UWE;
+
+namespace SubnauticaTelemetry.Patches
+{
+    [HarmonyPatch(typeof(FreezeTime), nameof(FreezeTime.End))]
+    class FreezeTimeEndPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            Main.dataProcessor.running = true;
+        }
+    }
+}
