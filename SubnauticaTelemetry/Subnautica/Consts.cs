11a426489539cdcc595ac9fc58858c84e6fafc4d
Roker2 <dimaminko2000@gmail.com>
2022-12-13 00:59:45 +0300

    Subnautica: Consts: Add food and water contants

--- SubnauticaTelemetry/Subnautica/Consts.cs
+++ SubnauticaTelemetry/Subnautica/Consts.cs
@@ -4,5 +4,10 @@
     {
         public const float MinOceanDepth = 0;
         public const float MaxOceanDepth = 1500;
+
+        public const float LowFoodThreshold = SurvivalConstants.kLowFoodThreshold;
+        public const float CriticalFoodThreshold = SurvivalConstants.kCriticalFoodThreshold;
+        public const float LowWaterThreshold = SurvivalConstants.kLowWaterThreshold;
+        public const float CriticalWaterThreshold = SurvivalConstants.kCriticalWaterThreshold;
     }
 }
