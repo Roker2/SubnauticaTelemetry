f5e41732b35ae5656db59120c8dc2db142cdfbb9
Roker2 <dimaminko2000@gmail.com>
2022-12-12 14:08:02 +0300

    Patches: Combine FreezeTime patches to one class

---
+++ SubnauticaTelemetry/Patches/FreezeTimePatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using UWE;
+
+namespace SubnauticaTelemetry.Patches
+{
+    [HarmonyPatch(typeof(FreezeTime))]
+    internal class FreezeTimePatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("Begin")]
+        public static void BeginPostfix()
+        {
+            Main.dataProcessor.Running = false;
+            Main.dataProcessor.StopAllEvents();
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("End")]
+        public static void EndPostfix()
+        {
+            Main.dataProcessor.Running = true;
+        }
+    }
+}
