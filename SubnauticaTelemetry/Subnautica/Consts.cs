159ff93489aa240402ad5eba5bc04fc5eb2f2c01
Roker2 <dimaminko2000@gmail.com>
2022-12-13 00:56:11 +0300

    Subnautica: Consts: Change static to const

--- SubnauticaTelemetry/Subnautica/Consts.cs
+++ SubnauticaTelemetry/Subnautica/Consts.cs
@@ -2,7 +2,7 @@
 {
     class Consts
     {
-        public static float MinOceanDepth = 0;
-        public static float MaxOceanDepth = 1500;
+        public const float MinOceanDepth = 0;
+        public const float MaxOceanDepth = 1500;
     }
 }
