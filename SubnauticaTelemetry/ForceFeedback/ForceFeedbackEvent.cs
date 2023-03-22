952d5952b164fb4ab9f6c113ae0267896fa2c0a2
Roker2 <dimaminko2000@gmail.com>
2023-03-22 21:51:43 +0100

    ForceFeedback: ForceFeedbackEvent: Add position

--- SubnauticaTelemetry/ForceFeedback/ForceFeedbackEvent.cs
+++ SubnauticaTelemetry/ForceFeedback/ForceFeedbackEvent.cs
@@ -1,21 +1,25 @@
-﻿namespace SubnauticaTelemetry.ForceFeedback
+﻿using UnityEngine;
+
+namespace SubnauticaTelemetry.ForceFeedback
 {
     public struct ForceFeedbackEvent
     {
         public ForceFeedbackType Type;
         public float Multiplier;
         public bool Replay;
+        public Vector3 Position;
 
-        public ForceFeedbackEvent(ForceFeedbackType type, float multiplier, bool replay)
+        public ForceFeedbackEvent(ForceFeedbackType type, float multiplier, bool replay, Vector3 position = new Vector3())
         {
             Type = type;
             Multiplier = multiplier;
             Replay = replay;
+            Position = position;
         }
 
         public override string ToString()
         {
-            return $"Type: {Type}, Multiplier: {Multiplier}, Replay: {Replay}";
+            return $"Type: {Type}, Multiplier: {Multiplier}, Replay: {Replay}, Direction: {Position}";
         }
     }
 }
