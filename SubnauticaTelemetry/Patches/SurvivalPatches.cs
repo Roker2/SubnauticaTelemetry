1f824bfa3d1f17381cd6460283c40be9811ebe95
Roker2 <dimaminko2000@gmail.com>
2023-03-22 22:47:46 +0100

    Patches: SurvivalPatches: Init

---
+++ SubnauticaTelemetry/Patches/SurvivalPatches.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace SubnauticaTelemetry.Patches
+{
+    [HarmonyPatch(typeof(Survival))]
+    internal class SurvivalPatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("UpdateHunger")]
+        public static void UpdateHungerPostfix(Survival __instance)
+        {
+            if (GameModeUtils.RequiresSurvival() && !__instance.freezeStats)
+            {
+                SubnauticaTelemetryPlugin.dataProcessor.ProcessFoodLevel(__instance.food);
+                SubnauticaTelemetryPlugin.dataProcessor.ProcessWaterLevel(__instance.water);
+            }
+        }
+    }
+}
