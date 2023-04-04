af3c5e992c64bf5bea20cba24ac64cafd1e6a6b9
Roker2 <dimaminko2000@gmail.com>
2023-04-04 18:29:42 +0200

    SubnauticaTelemetryPlugin: Add Log

--- SubnauticaTelemetry/SubnauticaTelemetryPlugin.cs
+++ SubnauticaTelemetry/SubnauticaTelemetryPlugin.cs
@@ -4,6 +4,7 @@ using SMLHelper.V2.Handlers;
 using SubnauticaTelemetry.Configuration;
 using SubnauticaTelemetry.Subnautica;
 using SubnauticaTelemetry.ForceFeedback;
+using BepInEx.Logging;
 
 namespace SubnauticaTelemetry
 {
@@ -14,6 +15,8 @@ namespace SubnauticaTelemetry
         private const string PluginName = "Subnautica Telemetry Library";
         private const string PluginVersion = "1.0.1";
 
+        internal static ManualLogSource Log;
+
         internal static Config Config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
 
         internal static DataProcessor dataProcessor = new DataProcessor();
@@ -21,6 +24,7 @@ namespace SubnauticaTelemetry
         private void Start()
         {
             new Harmony(PluginGUID).PatchAll();
+            Log = Logger;
         }
 
         /// <summary>
