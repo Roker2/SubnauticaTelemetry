2df95a434535765899ad26ca061e5f316c010852
Roker2 <dimaminko2000@gmail.com>
2023-04-13 15:07:44 +0200

    Patches: ScannerToolPatches: Init

---
+++ SubnauticaTelemetry/Patches/ScannerToolPatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace SubnauticaTelemetry.Patches
+{
+    [HarmonyPatch(typeof(ScannerTool))]
+    internal class ScannerToolPatches
+    {
+        private static bool isPlayerBeingScanned = false;
+
+        [HarmonyPostfix]
+        [HarmonyPatch("Scan")]
+        public static void ScanPostfix(ScannerTool __instance, PDAScanner.Result __result)
+        {
+            PDAScanner.ScanTarget scanTarget = PDAScanner.scanTarget;
+            if (!scanTarget.isValid)
+                return;
+            if (scanTarget.isPlayer && __result == PDAScanner.Result.Scan)
+            {
+                SubnauticaTelemetryPlugin.dataProcessor.ProcessSelfScanning();
+            }
+        }
+    }
+}
